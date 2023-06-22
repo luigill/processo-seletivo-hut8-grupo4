a00f5559001e1e3e390a692a7057d9eaf1871919
Marcus Schebek <marcusschebek@gmail.com>
2023-06-22 09:34:22 -0300

    Alteração nas rotas para a criação da page Categorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Categorias from "./pages/Categorias";
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/categorias" element={<Categorias/>} />
+
       </Routes>
     </Router>
   );
---
+++ src/pages/Categorias/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FooterMenu from "../../Components/Footer";
+import NavMenu from "../../Components/Menu";
+import "./styles.scss";
+
+export default function Categorias(){
+    return(
+        <>
+        <NavMenu/>
+        <h1>Categorias</h1>
+        <FooterMenu/>
+        </>
+    );
+}
