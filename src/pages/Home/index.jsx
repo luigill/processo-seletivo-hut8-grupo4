e18b9eaa2fd3b1a853c24382fe2d0308bc9075d5
Marcus Schebek <marcusschebek@gmail.com>
2023-06-22 13:38:06 -0300

    Home provisória

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,13 @@ export default function Home(){
     return(
         <>
         <NavMenu/>
-        
+        <div className="homeBanner">
+            <div className="contentBanner">
+            <h1>Empresa Junior da <br/> Computação - UFPel</h1>
+            <p>Desenvolvemos soluções tecnológicas personalizadas e inovadoras por um melhor custo benefício, aproximando jovens universitários do mercado de trabalho.</p>
+            <a href="https://www.instagram.com/hut8_jr/" target="_blank" rel="noreferrer"><button className="ButtonHome">Entre para a Hut!</button></a>
+            </div>
+        </div>
         <FooterMenu/>
         </>
     );
