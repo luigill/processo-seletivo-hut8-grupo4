07f47bc29fa9079c33eb32a979976274b6abf7d9
Marcus Schebek <marcusschebek@gmail.com>
2023-06-29 13:20:01 -0300

    menu e footer responsivo

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -3,61 +3,71 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import user from "../../assets/User.svg";
 import CarrinhoDeCompra from "../../assets/Shopping_Cart.svg";
-import LoginModal from "../../Components/LoginModal"
+import LoginModal from "../../Components/LoginModal";
+import menuResponsivo from "../../assets/menu-buguer-open.svg";
 import "./styles.scss";
-
 
 export default function NavMenu() {
   const [currentPage, setCurrentPage] = useState("");
   const [modal, setModal] = useState(false);
-  
+  const [showResponsiveMenu, setShowResponsiveMenu] = useState(false);
 
   useEffect(() => {
     const currentPath = window.location.pathname.split("/")[1];
     setCurrentPage(currentPath);
   }, []);
 
+  const toggleResponsiveMenu = () => {
+    setShowResponsiveMenu(!showResponsiveMenu);
+  };
+
   return (
     <>
-    <header>
-      <Link to="/" className="navLogo">
-        <img src={logo} alt="logo" />
-      </Link>
-      <ul>
-        <li>
-          <Link to="/" className={currentPage === "" ? "active" : ""}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/produtos"
-            className={currentPage === "produtos" ? "active" : ""}
-          >
-            Produtos
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/nossa-equipe"
-            className={currentPage === "nossa-equipe" ? "active" : ""}
-          >
-            Nossa Equipe
-          </Link>
-        </li>
-        <li>
-          <a href="#">
-            <img className="iconMenu" src={CarrinhoDeCompra} alt="" />
-          </a>
-        </li>
-        <li onClick={() => setModal(true)}>
-          <a href="#">
-            <img className="iconMenu" src={user} alt="User" />
-          </a>
-        </li>
-      </ul>
-    </header>
-    {modal && <LoginModal closeModal={() => setModal(false)} />}
-  </>
+      <header>
+        <Link to="/" className="navLogo">
+          <img src={logo} alt="logo" />
+        </Link>
+        <button
+          className="buttonMobileMenu"
+          onClick={toggleResponsiveMenu}
+        >
+          <img src={menuResponsivo} alt="Menu" />
+        </button>
+        <ul className={showResponsiveMenu ? "responsive-menu" : "desktop-menu"}>
+          <li>
+            <Link to="/" className={currentPage === "" ? "active" : ""}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/produtos"
+              className={currentPage === "produtos" ? "active" : ""}
+            >
+              Produtos
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/nossa-equipe"
+              className={currentPage === "nossa-equipe" ? "active" : ""}
+            >
+              Nossa Equipe
+            </Link>
+          </li>
+          <li>
+            <a href="#">
+              <img className="iconMenu" src={CarrinhoDeCompra} alt="" />
+            </a>
+          </li>
+          <li onClick={() => setModal(true)}>
+            <a href="#">
+              <img className="iconMenu" src={user} alt="User" />
+            </a>
+          </li>
+        </ul>
+      </header>
+      {modal && <LoginModal closeModal={() => setModal(false)} />}
+    </>
   );
 }
